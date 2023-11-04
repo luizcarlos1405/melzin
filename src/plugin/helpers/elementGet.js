8e5c4a3e9ae0f8a6aa69696d182fa436bacc7b6f
Luiz C. L. G. de Oliveira <luizcarlos1405@pop-os.localdomain>
2023-11-04 18:45:24 -0400

    avoid adding undefined to path

--- src/plugin/helpers/elementGet.js
+++ src/plugin/helpers/elementGet.js
@@ -2,7 +2,7 @@ import { getAt } from "./getAt";
 import { getElementDataPath } from "./getElementDataPath";
 import { joinPath } from "./joinPath";
 
-export const elementGet = (el, path) => {
+export const elementGet = (el, path = "") => {
   const pathFromRoot = joinPath(getElementDataPath(el), path);
   return getAt(pathFromRoot);
 };
