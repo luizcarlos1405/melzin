55e50605bd5d2c43cc4cb1c3d732843ea6d9cc11
Luiz C. L. G. de Oliveira <luizcarlos1405@pop-os.localdomain>
2023-11-04 18:58:37 -0400

    parse stored state with object as fallback

--- src/plugin/debug/exposeDevHelpers.js
+++ src/plugin/debug/exposeDevHelpers.js
@@ -3,6 +3,7 @@ import { setAt } from "../helpers/setAt";
 import { getElementDataPath } from "../helpers/getElementDataPath";
 import { scanStructure } from "../helpers/scanStructure";
 import { queryWholeHtml } from "../helpers/queryWholeHtml";
+import { parseJsonWithFallback } from "../helpers/parseJsonWithFallback";
 
 export const exposeDevHelpers = () => {
   window.$getAt = getAt;
@@ -17,8 +18,9 @@ export const exposeDevHelpers = () => {
   window.$getStoredState = () =>
     localStorage.getItem(window.$localStorageStateKey);
   window.$loadStoredState = () => {
-    const savedState = JSON.parse(
+    const savedState = parseJsonWithFallback(
       localStorage.getItem(window.$localStorageStateKey),
+      {},
     );
     if (savedState) {
       // Alpine.app.state.root = savedState;
